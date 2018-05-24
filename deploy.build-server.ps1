e3064da917a8b312744b1fb76c0cc5504a278455
Shabir Govani <Shabir.Govani.DfE@outlook.com>
2018-05-24 15:26:14 +0100

    remove lock

--- deploy.build-server.ps1
+++ deploy.build-server.ps1
@@ -15,7 +15,7 @@ param(
 
 try {
 
-    Install-Module xDisk, xPendingReboot, xNetworking, xPSDesiredStateConfiguration -Scope CurrentUser -Force
+    Install-Module xDisk, xPendingReboot, xNetworking, xPSDesiredStateConfiguration -Scope CurrentUser
 
      # --- Install Dependencies From PSGallery
      Write-Host "Installing DSC Dependencies"
