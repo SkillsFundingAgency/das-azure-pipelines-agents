582fac62fb0bddcd739a5743e56c03fa913d5a7c
HussainsGitHub <85938019+HussainsGitHub@users.noreply.github.com>
2024-10-16 16:18:09 +0100

    DASD-11212 - Setup Autoscaling for Build and Deploy node pools

--- Windows/Build/Image/Start.ps1
+++ Windows/Build/Image/Start.ps1
@@ -35,31 +35,15 @@ if ($Env:AZP_WORK -and -not (Test-Path Env:AZP_WORK)) {
 	New-Item $Env:AZP_WORK -ItemType directory | Out-Null
 }
 
-New-Item "\azp\agent" -ItemType directory | Out-Null
-
-# Let the agent ignore the token env variables
+#Let the agent ignore the token env variables
 $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
 
-Set-Location agent
+#Use the pre-downloaded agent from Dockerfile at C:\azp-agent
+Set-Location C:\azp-agent
 
-Write-Host "1. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
-
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
-$package = Invoke-RestMethod -Headers @{Authorization = ("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
-$packageUrl = $package[0].Value.downloadUrl
-
-Write-Host $packageUrl
-
-Write-Host "2. Downloading and installing Azure Pipelines agent..." -ForegroundColor Cyan
-
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($packageUrl, "$(Get-Location)\agent.zip")
-
-Expand-Archive -Path "agent.zip" -DestinationPath "\azp\agent"
+Write-Host "1. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
 
 try {
-	Write-Host "3. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
-
 	.\config.cmd --unattended `
 		--agent "$(if (Test-Path Env:AZP_AGENT) { ${Env:AZP_AGENT} } else { ${Env:POD_NAME} })" `
 		--url "$(${Env:AZP_URL})" `
@@ -69,7 +53,7 @@ try {
 		--work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
 		--replace
 
-	Write-Host "4. Running Azure Pipelines agent..." -ForegroundColor Cyan
+	Write-Host "2. Running Azure Pipelines agent..." -ForegroundColor Cyan
 
 	.\run.cmd --once
 }
