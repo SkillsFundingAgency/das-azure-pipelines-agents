d218eb69301dec98af1ca014040676db236ce983
HussainsGitHub <85938019+HussainsGitHub@users.noreply.github.com>
2024-10-30 21:24:01 +0000

    DASD-11212 - Update Windows Deploy Docker Image to directly download agent

--- Windows/Deploy/Image/Start.ps1
+++ Windows/Deploy/Image/Start.ps1
@@ -19,6 +19,7 @@ if ($Env:AZP_WORK -and -not (Test-Path Env:AZP_WORK)) {
 	New-Item $Env:AZP_WORK -ItemType directory | Out-Null
 }
 
+
 # Let the agent ignore the token env variables
 $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
 
